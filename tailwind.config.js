029b989e02aa8389b5b6c927a31e86e0b8723ea5
Behruz <marsithb2022@gmail.com>
2024-11-29 20:49:05 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        screens: {
+            'tablet': '768px',
+            // => @media (min-width: 640px) { ... }
+
+            'desktop': '1440px',
+                // => @media (min-width: 1024px) { ... }
+        },
+    }
+}
